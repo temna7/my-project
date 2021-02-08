bfd017400e661d7d83e16e60900a4a3dd36b7963
temna7 <77911360+temna7@users.noreply.github.com>
2021-02-07 21:05:29 -0800

    updated pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [{ name: 'Bulbasaur', height: 7, types:['grass','poision']}];
+let pokemonList = [
+    { name: 'Bulbasaur', height: 7, types: ['grass', 'poision'] },
+    { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
+    { name: 'Blastoise', height: 1.6, types: ['steel', 'water'] }
+];
