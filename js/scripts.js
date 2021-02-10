81636ad28988e2ea1f6963a434f0486d724602cf
temna7 <77911360+temna7@users.noreply.github.com>
2021-02-10 18:23:14 -0500

    update on pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let pokemonList = [
     { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
     { name: 'Blastoise', height: 1.6, types: ['steel', 'water'] }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    console.log(pokemonList[i].name, pokemonList[i].height)
+    if (pokemonList[i].height >= 1) {
+        console.log('Wow that is Big')
+
+    }
+}
+
