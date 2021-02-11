3c170239595729e58b5058d8c2a785ef3ef37bf3
Yohannes Maddie <temnasera256@gmail.com>
2021-02-11 17:52:36 -0500

    pokemonList /index.html
    document.write

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poision'] },
+    { name: 'Bulbasaur', height: 3, types: ['grass', 'poision'] },
     { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
     { name: 'Blastoise', height: 1.6, types: ['steel', 'water'] }
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
     console.log(pokemonList[i].name, pokemonList[i].height)
+    document.write(pokemonList[i].name, pokemonList[i].height)
     if (pokemonList[i].height >= 1) {
-        console.log('Wow that is Big')
+        console.log('Wow that is Big!')
+        document.write('Wow that is Big!')
 
     }
 }
-
