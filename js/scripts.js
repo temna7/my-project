701faa705489d00190d575e9abdd734248450f2f
Yohannes Maddie <temnasera256@gmail.com>
2021-02-15 15:34:05 -0500

    Update scripts.js
    mathematical operation

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,30 @@ let pokemonList = [
     }
   }
   
-  
+  function add(a, b) {
+    return a+b;
+};
+console.log(add(6,7))
+
+function subtract(num1, num2) {
+    return num2-num1;
+};
+console.log(subtract(2,9))
+
+function multiply(vol,vol2) {
+    return vol*vol;
+}
+console.log(multiply(4,3))
+
+function divide(dividend, divisor){
+    if(divisor === 0){
+      return "not allowed."
+    }else{
+      let result = dividend / divisor;
+      return result;
+    }
+  }
+console.log(divide(16,4))
   
   
   
