ad27b61ef8035c93ce70246887e06e0dcaf7163e
Yohannes Maddie <temnasera256@gmail.com>
2021-02-15 15:30:48 -0500

    Update scripts.js
    Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,18 @@ let pokemonList = [
     { name: 'Bulbasaur', height: 3, types: ['grass', 'poision'] },
     { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
     { name: 'Blastoise', height: 1.6, types: ['steel', 'water'] }
-];
-
-for (let i = 0; i < pokemonList.length; i++) {
-    console.log(pokemonList[i].name, pokemonList[i].height)
-    document.write(pokemonList[i].name, pokemonList[i].height)
-    if (pokemonList[i].height >= 1) {
-        console.log('Wow that is Big!')
-        document.write('Wow that is Big!')
-
+  ];
+  
+  for (let i = 0; i < pokemonList.length; i++) {
+    document.write("<p>" + pokemonList[i].name + "</p>" + '</p>');
+    if (pokemonList[i].height >= 3) {
+      console.log('Wow that is Big!')
+      document.write('Wow that is Big!')
+  
     }
-}
+  }
+  
+  
+  
+  
+  
