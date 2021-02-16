8f4fd017cdfdf364c6e115a1a9296e2667495e54
Yohannes Maddie <temnasera256@gmail.com>
2021-02-16 14:51:00 -0500

    Pokemon Update_ wow

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,17 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 3, types: ['grass', 'poision'] },
-    { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
-    { name: 'Blastoise', height: 1.6, types: ['steel', 'water'] }
-  ];
-  
-  for (let i = 0; i < pokemonList.length; i++) {
-    document.write("<p>" + pokemonList[i].name + "</p>" + '</p>');
-    if (pokemonList[i].height >= 3) {
-      console.log('Wow that is Big!')
-      document.write('Wow that is Big!')
-  
-    }
+  { name: 'Bulbasaur', height: 3, types: ['grass', 'poision'] },
+  { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
+  { name: 'Blastoise', height: 1.6, types: ['steel', 'water'] }
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 3) {
+    console.log('Wow that is Big!')
+    document.write('<p>' + pokemonList[i].name + ' ' + "Wow that is Big" + '</p>')
   }
-  
-  function add(a, b) {
-    return a+b;
-};
-console.log(add(6,7))
+  else {
+    document.write('<p>' + pokemonList[i].name + '</p>')
 
-function subtract(num1, num2) {
-    return num2-num1;
-};
-console.log(subtract(2,9))
-
-function multiply(vol,vol2) {
-    return vol*vol;
+  }
 }
-console.log(multiply(4,3))
 
-function divide(dividend, divisor){
-    if(divisor === 0){
-      return "not allowed."
-    }else{
-      let result = dividend / divisor;
-      return result;
-    }
-  }
-console.log(divide(16,4))
-  
-  
-  
