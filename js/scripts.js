fc7861eb56176481225ecb0603e798337c8db0ed
Yohannes Maddie <temnasera256@gmail.com>
2021-02-19 15:27:03 -0500

    for-each loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
-let pokemonList = [
-  { name: 'Bulbasaur', height: 3, types: ['grass', 'poision'] },
+let pokemonRepository = (function () {
+  let pokemonList = [{ name: 'Bulbasaur', height: 3, types: ['grass', 'poision'] },
   { name: 'Ivysaur', height: 1, types: ['grass', 'psychic'] },
-  { name: 'Blastoise', height: 1.6, types: ['steel', 'water'] }
-];
+  { name: 'Blastoise', height: 1.6, types: ['steel', 'water'] }];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height >= 3) {
+  return {
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function () {
+      return pokemonList;
+    }
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  if (pokemon.height >= 3) {
     console.log('Wow that is Big!')
-    document.write('<p>' + pokemonList[i].name + ' ' + "Wow that is Big" + '</p>')
+    document.write('<p>' + pokemon.name + ' ' + "Wow that is Big" + '</p>')
   }
   else {
-    document.write('<p>' + pokemonList[i].name + '</p>')
-
+    document.write('<p>') + pokemon.name + '</P>'
   }
-}
+});
+
+
 
